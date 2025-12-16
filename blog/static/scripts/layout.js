1a2fd9dc40e1a5e1a018d313cc81422b5d98b7b4
Michael Ampomah <ampomahmichael321@gmail.com>
2025-12-16 10:01:24 +0000

    removed the contact link pop up

--- blog/static/scripts/layout.js
+++ blog/static/scripts/layout.js
@@ -15,17 +15,3 @@ navLinks.forEach((link) => {
     hiddenMenu.style.transform = "translateX(200em)";
   });
 });
-
-const contactLink = document.querySelector(".contact-link");
-const contactPopUp = document.querySelector(".contact-pop-up");
-contactLink.addEventListener("click", () => {
-  if (contactPopUp.style.opacity !== "1") {
-    contactPopUp.style.opacity = "1";
-  }
-});
-const body = document.querySelector("body");
-body.addEventListener("click", (e) => {
-  if (contactPopUp.style.opacity === "1" && e.target !== contactLink) {
-    contactPopUp.style.opacity = "0";
-  }
-});
